d7d98f9de07c4c85c0b8bad26f460806a343fc4d
Engjell Ramadani <eramadani@wpi.edu>
2022-04-24 21:37:02 -0400

    Updating table

--- src/main/java/edu/wpi/teamY/APITableController.java
+++ src/main/java/edu/wpi/teamY/APITableController.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 import javax.swing.*;
 import javax.swing.text.TableView;
@@ -18,18 +19,18 @@ public class APITableController {
   @FXML private MFXButton deleteButton;
   @FXML private MFXButton modifyButton;
   @FXML private TableView tableView;
-  /*
-    @FXML TableColumn location = new TableColumn(0);
-    @FXML TableColumn priority = new TableColumn(1);
-    @FXML TableColumn status = new TableColumn(2);
-    @FXML TableColumn employee = new TableColumn(3);
-    @FXML TableColumn notes = new TableColumn(4);
-  */
+
+  @FXML TableColumn<String, String> location = new TableColumn<>();
+  @FXML TableColumn<String, String> priority = new TableColumn<>();
+  @FXML TableColumn<String, String> status = new TableColumn<>();
+  @FXML TableColumn<String, String> employee = new TableColumn<>();
+  @FXML TableColumn<String, String> notes = new TableColumn<>();
+
   @FXML
   public void addNewButton() {
     try {
       FXMLLoader fxmlLoader = new FXMLLoader();
-      FXMLLoader.load(Objects.requireNonNull(getClass().getResource("APITable.fxml")));
+      FXMLLoader.load(Objects.requireNonNull(getClass().getResource("API.fxml")));
       /*
        * if "fx:controller" is not set in fxml
        * fxmlLoader.setController(NewWindowController);
