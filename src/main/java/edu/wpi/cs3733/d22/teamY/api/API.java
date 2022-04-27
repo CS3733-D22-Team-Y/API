e08ee3b4d9952bd5d1229fceeeeb8741897cb20a
Ben Schmitt <bjschmitt@wpi.edu>
2022-04-26 21:47:23 -0400

    fized da gawed damn apoee

--- src/main/java/edu/wpi/cs3733/d22/teamY/api/API.java
+++ src/main/java/edu/wpi/cs3733/d22/teamY/api/API.java
@@ -7,7 +7,18 @@ public class API {
     // CSVRW.loadFromCSV(EntryType.LOCATION);
     // CSVRW.loadFromCSV(EntryType.EMPLOYEE);
     // DBManager.save(new SecurityService("0", "0", "0", "0", "0"));
-    // run(100, 100, 620, 450, "", "destLocationID", "");
+    if (args.length < 2) {
+      run(100, 100, 620, 450, "", "destLocationID", "");
+    } else {
+      run(
+          Integer.parseInt(args[0]),
+          Integer.parseInt(args[1]),
+          Integer.parseInt(args[2]),
+          Integer.parseInt(args[3]),
+          args[4],
+          args[5],
+          args[6]);
+    }
   }
 
   public static void run(
